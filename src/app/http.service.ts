407975d3ef0d08dab6e11145b256107ddbb01d7c
iamjigz <jigzimon@gmail.com>
2019-08-08 06:20:23 +0800

    Update

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Contact } from './contact';
-import { retry, timeout, catchError } from 'rxjs/operators';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
